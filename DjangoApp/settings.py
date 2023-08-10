3b8927fef37c387d492140c8315c38a5e19704f7
MAX <maxym.baluk@gmail.com>
2023-08-10 22:31:56 +0300

    final_6

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 INTERNAL_IPS = [
     '127.0.0.1'
--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from DjangoApp import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ToDoList.urls')),
-    path('user/', include('users.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('ToDoList.urls')),
+                  path('user/', include('users.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
